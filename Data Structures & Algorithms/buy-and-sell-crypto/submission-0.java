e71c1e1170276c2f639cbfcccebef6893032004b
AmaanS-01 <mohdamaanuddins06@gmail.com>
2026-09-23 19:06:05 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i=0;
+        int j=i+1;
+        int profit=0;
+        while(j<prices.length){
+            if(prices[j]<=prices[i]){
+                i=j;
+                j++;
+            }
+            else{
+                profit=Math.max(profit,prices[j]-prices[i]);
+                j++;
+            }
+        }
+        return profit;
+    }
+}
